4a55c087777580ec044197addf22d6394064cc03
Ayuzzz127 <yuz127@ucsd.edu>
2022-04-21 09:18:14 -0700

    test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,22 @@
 import static org.junit.Assert.*;
+import java.beans.Transient;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.*;
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+    @Test
+    public void Link() throws IOException{
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(links, List.of("https://something.com", "some-page.html"));
     }
 }
