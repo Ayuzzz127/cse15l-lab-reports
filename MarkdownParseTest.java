b6b564e02e896eb17d1795a0b50402884e0db0b2
Ayuzzz127 <yuz127@ucsd.edu>
2022-04-21 09:51:18 -0700

    test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -14,7 +14,7 @@ public class MarkdownParseTest {
     }
     @Test
     public void Link() throws IOException{
-        Path fileName = Path.of("test-file.md");
+        Path fileName = Path.of("test-file8.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
         assertEquals(links, List.of("https://something.com", "some-page.html"));
